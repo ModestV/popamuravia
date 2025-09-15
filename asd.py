a400001958754296297c254dd43353c7e1bc1b6a
ModestV <vladpolovinka@mail.ru>
2025-09-15 23:52:02 +0500

    Add files via upload

---
+++ asd.py
@@ -0,0 +1,33 @@
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if b == 0:
+        raise ValueError("Деление на ноль невозможно")
+    return a / b
+
+
+if __name__ == "__main__":
+    print("Простой калькулятор")
+    print("Доступные операции: +, -, *, /")
+
+    a = float(input("Введите первое число: "))
+    op = input("Операция (+ - * /): ")
+    b = float(input("Введите второе число: "))
+
+    if op == "+":
+        print("Результат:", add(a, b))
+    elif op == "-":
+        print("Результат:", sub(a, b))
+    elif op == "*":
+        print("Результат:", mul(a, b))
+    elif op == "/":
+        print("Результат:", div(a, b))
+    else:
+        print("Неизвестная операция")
